d2cb4a238a63863d7d60514b0f9637cd48de253c
Roger <103703041@nccu.edu.tw>
2017-05-02 18:09:25 +0800

    Change the APP theme color and prepare to set Watson Voice Text to Speech on the IBM Cloud

--- app/src/main/java/com/example/lawtalk/MainActivity.java
+++ app/src/main/java/com/example/lawtalk/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lawtalk;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,8 +13,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends Navigation_BaseActivity{
+
+    TextView textView;
+    EditText editText;
+    Button button;
+
+    private class WatsonTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... textToSpeak){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText("running the Watson thread");
+                }
+            });
+            return "text to speech done";
+        }
+        @Override
+        protected void onPostExecute(String result){
+            textView.setText("TTS status: " + result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +47,21 @@ public class MainActivity extends Navigation_BaseActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("LawTalk - 首頁");
         setUpAll(toolbar);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                System.out.println("the text to speech: " + editText.getText());
+                textView.setText("TTS: " + editText.getText());
+
+                WatsonTask task = new WatsonTask();
+                task.execute(new String[]{});
+            }
+        });
+
     }
 }
