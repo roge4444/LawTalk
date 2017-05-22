39dca4e1d73bf9d0406ef82e7184004cf7537dcd
Roger <103703041@nccu.edu.tw>
2017-05-23 06:12:04 +0800

    Add WatBot into LawTalk as the ask function.

--- app/src/main/java/com/example/lawtalk/SplashActivity.java
+++ app/src/main/java/com/example/lawtalk/SplashActivity.java
@@ -21,6 +21,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 1750);
+        }, 1800);
     }
 }
